b6cfafa9f262c42a5b2b2fec5dbcf59a0a4ee79a
Daniel Ranchal <danitico98@gmail.com>
2019-02-23 17:43:40 +0100

    Here comes the graphics

--- P1/Debug/gnuplot.sh
+++ P1/Debug/gnuplot.sh
@@ -1,1 +1,3 @@
-gnuplot -persist -e "plot 'Soluciones.txt' using 1 with linespoints pi 10000, 'solutions.eps' using 3 with linespoints pi 10000"
+#!/bin/bash
+gnuplot -persist -e "set output '5Knapsacks_200_2.eps' ; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness';
+		     plot '5Knapsacks.txt' using 11 t 'Best average in iteration x' with linespoints pi 10000, '5Knapsacks.txt' using 12 t 'Best average until iteration x'with linespoints pi 10000"
--- P1/src/MQKPEvaluator.cpp
+++ P1/src/MQKPEvaluator.cpp
@@ -13,6 +13,13 @@
 #include "MQKPInstance.h"
 #include "MQKPEvaluator.h"
 
+	/*
+	 *  Calculate the fitness of the solution in the following way:
+	 *   1. Obtain the maximum violation of the knapsack capacities, by using the corresponding function.
+	 *   2. If it is higher than 0, then return the maximum violation multiplied by -1.
+	 *   3. If not, the return the sum of the individual and share profits using the corresponding function.
+	 */
+
 double MQKPEvaluator::computeFitness(MQKPInstance &instance, MQKPSolution &solution){
 
 	double fitness = 0;
@@ -24,12 +31,6 @@ double MQKPEvaluator::computeFitness(MQKPInstance &instance, MQKPSolution &solut
 	else{
 		fitness = instance.getSumProfits(solution);
 	}
-	/*
-	 *  Calculate the fitness of the solution in the following way:
-	 *   1. Obtain the maximum violation of the knapsack capacities, by using the corresponding function.
-	 *   2. If it is higher than 0, then return the maximum violation multiplied by -1.
-	 *   3. If not, the return the sum of the individual and share profits using the corresponding function.
-	 */
 
 	return fitness;
 }
