664a2492cf1592225b545cdca38f6a53b53bda54
Daniel Ranchal <danitico98@gmail.com>
2019-03-09 20:05:02 +0100

    Plots for 3 knapsacks and all the instances. 1 means 25% of density and 2 means 75% of density

--- P2/Debug/gnuplot.sh
+++ P2/Debug/gnuplot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gnuplot -persist -e "set output '3Knapsacks_100_First_1.eps' ; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness';
-		     plot '3Knapsacks_100_25density.txt' using 1 t 'Current average in step x' with linespoints pi 10000, '3Knapsacks_100_25density.txt' using 2 t 'Best average until iteration x' with linespoints pi 10000"
+gnuplot -persist -e "set key right bottom; set output '3Knapsacks_200_Best_2.eps' ; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness';
+		     plot '3Knapsacks_200_75density.txt' using 3 t 'Current average in step x' w l lw 3, '3Knapsacks_200_75density.txt' using 4 t 'Best average until iteration x' w l lw 3"
