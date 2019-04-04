61b1310105dfd846e18c56ec80865a89dd2b931f
Daniel Ranchal <danitico98@gmail.com>
2019-04-04 14:58:29 +0200

    Plots are done :cat:

--- P3/Debug/gnuplot.sh
+++ P3/Debug/gnuplot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-gnuplot -persist -e "set key right bottom; set output 'prueba.eps' ; set logscale xy 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot 'prueba.txt' using 1 t 'Current SA' w l lw 1, 'prueba.txt' using 2 t 'Best SA' w l lw 1"
+gnuplot -persist -e "set key right bottom; set output '$1_SA.eps' ; set logscale y 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot '$1' using 1 t 'Current' w l lw 1, '$1' using 2 t 'Best' w l lw 1"
 
-gnuplot -persist -e "set key right bottom; set output 'prueba1.eps' ; set logscale xy 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot 'prueba1.txt' using 1 t 'Current SA' w l lw 1, 'prueba1.txt' using 2 t 'Best SA' w l lw 1"
+gnuplot -persist -e "set key right bottom; set output '$1_TS.eps' ; set logscale y 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot '$1' using 3 t 'Current' w l lw 1, '$1' using 4 t 'Best' w l lw 1"
 
-gnuplot -persist -e "set key right bottom; set output 'pruebaIG.eps' ; set logscale xy 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot 'prueba.txt' using 7 t 'Current IG' w l lw 1, 'prueba.txt' using 8 t 'Best IG' w l lw 1"
+gnuplot -persist -e "set key right bottom; set output '$1_Grasp.eps' ; set logscale y 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot '$1' using 5 t 'Current' w l lw 1, '$1' using 6 t 'Best' w l lw 1"
 
-gnuplot -persist -e "set key right bottom; set output 'pruebaIG1.eps' ; set logscale xy 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot 'prueba1.txt' using 7 t 'Current IG' w l lw 1, 'prueba1.txt' using 8 t 'Best IG' w l lw 1"
+gnuplot -persist -e "set key right bottom; set output '$1_IG.eps' ; set logscale y 10; set terminal postscript eps color; set xlabel 'Execution step'; set ylabel 'Fitness'; plot '$1' using 7 t 'Current' w l lw 1, '$1' using 8 t 'Best' w l lw 1"
