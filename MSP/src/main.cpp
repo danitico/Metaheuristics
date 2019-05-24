a2641f1ccfede726e009c4862cbc553ed7fed5c0
Daniel Ranchal <danitico98@gmail.com>
2019-05-24 12:01:30 +0200

    refactoring code

--- MSP/src/main.cpp
+++ MSP/src/main.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : aux.cpp
-// Author      : 
-// Version     :d
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 #include <MSPInstance.h>
 #include <MSPSolution.h>
 #include <MSPRandomSolution.h>
@@ -14,6 +7,27 @@ using namespace std;
 extern unsigned int numSeeds;
 extern unsigned int seeds[];
 
+void runRandomFunction(MSPInstance instance, MSPSolution solution){
+	int r=0;
+	srand(seeds[r]); r++;
+	MSPRandomSolution::genRandomSol(instance, solution);
+
+	double Bestsol=instance.computeFitness(solution);
+	MSPSolution bestSolution=solution;
+	std::cout<<Bestsol<<" "<<Bestsol<<std::endl;
+    for(int i=0;i<10000;i++){
+    	srand(seeds[r]);seeds[r]+=rand()%100; r=(r+1)%numSeeds;
+    	MSPRandomSolution::genRandomSol(instance, solution);
+    	double currentsol=instance.computeFitness(solution);
+
+    	if(currentsol>Bestsol){
+    		bestSolution=solution;
+    		Bestsol=currentsol;
+    	}
+    	std::cout<<currentsol<<" "<<Bestsol<<std::endl;
+    }
+}
+
 int main(int argc, char** argv) {
 	if(argc<2){
 		std::cout<<"You must include at least one instance file"<<std::endl;
@@ -22,23 +36,7 @@ int main(int argc, char** argv) {
 	MSPInstance instance;
 	instance.readInstance(argv[1]);
 	MSPSolution solution(instance.getNumberOfLiterals());
-	int r=0;
-	srand(seeds[r]); r++;
-	MSPRandomSolution::genRandomSol(instance, solution);
 
-	double sol=instance.computeFitness(solution);
-	MSPSolution bestSolution=solution;
-	std::cout<<sol<<" "<<sol<<std::endl;
-    for(int i=0;i<10000;i++){
-    	srand(seeds[r]);seeds[r]+=rand()%100; r=(r+1)%numSeeds;
-    	MSPRandomSolution::genRandomSol(instance, solution);
-    	double newsol=instance.computeFitness(solution);
-
-    	if(newsol>sol){
-    		bestSolution=solution;
-    		sol=newsol;
-    	}
-    	std::cout<<newsol<<" "<<sol<<std::endl;
-    }
+	runRandomFunction();
 
 }
